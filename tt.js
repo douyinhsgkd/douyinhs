8de11c1d29bb04fc85ddb9e2ca90f0b90f3e333c
douyinhsgkd <darongshu223@gmail.com>
2024-12-31 16:08:36 +0800

    Create tt.js

---
+++ tt.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5046ecfc6b816916d5c5794ee8389949";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
